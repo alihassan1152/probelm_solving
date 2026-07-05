85ba04012702d1ec37d27fecb4d45e7efc9c428a
Hassan <hasanstay4u@gmail.com>
2026-07-05 23:44:40 +0500

    06_remove_duplicates_in_list.py practice file

---
+++ 06_remove_duplicates_in_list.py
@@ -0,0 +1,32 @@
+# Remove duplicates in List
+
+fibbonacchi = [1,2,3,4,4,5,6,7,8,9,9]
+
+unique = []
+
+for n in fibbonacchi:
+    if n not in unique:
+        unique.append(n)
+print(unique)
+
+
+# 2nd way
+
+elements = [1,2,3,4,4,5,6,6]
+
+print(list(dict.fromkeys(elements)))
+
+
+
+# 3rd way
+
+numbers = [3, 1, 2, 1, 3, 4]
+
+print(list(set(numbers)))           
+print(list(dict.fromkeys(numbers)))  
+
+unique = []
+for n in numbers:
+    if n not in unique:
+        unique.append(n)
+    print(f"n={n}, unique={unique}")  
