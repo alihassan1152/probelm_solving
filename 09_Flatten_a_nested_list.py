a2dfc6e2f49f15aa4c4e500905f710d2a42218dd
Hassan <hasanstay4u@gmail.com>
2026-07-06 15:58:27 +0500

    add 09_flatten_a_nested_list.py practice file

---
+++ 09_Flatten_a_nested_list.py
@@ -0,0 +1,10 @@
+# Nested List [[1,2],[3,4],[5,6]] ke elements ko single List [1,2,3,4,5,6] main convert karta hai
+
+nested = [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
+
+flat = []
+
+for sublist in nested:
+    for item in sublist:
+        flat.append(item)
+print(f"The final List is here after solve nested list problem: {flat}")
